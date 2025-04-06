60b039ba2b75f6c6059b599fc62738689c0ac07f
Mahi <157265828+mahipandcy@users.noreply.github.com>
2025-04-06 12:47:27 +0800

    Update setup.sql

--- backend_tests/db/setup.sql
+++ backend_tests/db/setup.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS contacts (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
-    phone_number VARCHAR(20) UNIQUE
+    phone_number VARCHAR(30) UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS messages (
